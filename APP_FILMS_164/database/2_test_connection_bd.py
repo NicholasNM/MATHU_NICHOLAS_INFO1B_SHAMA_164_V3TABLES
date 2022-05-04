b97fc435fcb4352269f16c3eb6a0f551ce54cadf
SmileUniverse <73386456+SmileUniverse@users.noreply.github.com>
2022-05-04 12:58:55 +0200

    ajout d'un select correct pour effectuer le test --- modifiation des select dans le fichier gestion_genres_crud.py avec des select concernant ma table personnes

--- APP_FILMS_164/database/2_test_connection_bd.py
+++ APP_FILMS_164/database/2_test_connection_bd.py
@@ -9,7 +9,9 @@ try:
     """
         Une seule requête pour montrer la récupération des données de la BD en MySql.
     """
-    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre FROM t_genre ORDER BY id_genre ASC"""
+    strsql_genres_afficher = """SELECT nom_personne
+                                FROM t_personnes
+                                WHERE nom_personne REGEXP '[A-Z]M[A-Z]';"""
 
     with DBconnection() as db:
         db.execute(strsql_genres_afficher)
--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -34,7 +34,7 @@ def genres_afficher(order_by, id_genre_sel):
         try:
             with DBconnection() as mc_afficher:
                 if order_by == "ASC" and id_genre_sel == 0:
-                    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre FROM t_genre ORDER BY id_genre ASC"""
+                    strsql_genres_afficher = """SELECT id_personnes, nom_personnes, datenaissance FROM t_personnes ORDER BY id_personnes ASC"""
                     mc_afficher.execute(strsql_genres_afficher)
                 elif order_by == "ASC":
                     # C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
@@ -43,11 +43,11 @@ def genres_afficher(order_by, id_genre_sel):
                     # donc, je précise les champs à afficher
                     # Constitution d'un dictionnaire pour associer l'id du genre sélectionné avec un nom de variable
                     valeur_id_genre_selected_dictionnaire = {"value_id_genre_selected": id_genre_sel}
-                    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre  FROM t_genre WHERE id_genre = %(value_id_genre_selected)s"""
+                    strsql_genres_afficher = """SELECT id_personnes, nom_personnes, datenaissance FROM t_personnes WHERE id_personnes = %(value_id_genre_selected)s"""
 
                     mc_afficher.execute(strsql_genres_afficher, valeur_id_genre_selected_dictionnaire)
                 else:
-                    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre  FROM t_genre ORDER BY id_genre DESC"""
+                    strsql_genres_afficher = """SELECT id_personnes, nom_personnes, datenaissance FROM t_personnes ORDER BY id_personnes  DESC"""
 
                     mc_afficher.execute(strsql_genres_afficher)
 
